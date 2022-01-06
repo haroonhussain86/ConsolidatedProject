2a21d1bd465a62c1536594f9f42ca108289e36e5
Haroon Hussain <haroonhussain86@yahoo.co.uk>
2022-01-06 15:32:15 +0000

    cucumber

--- src/test/java/AppFeatures/Reviews.feature
+++ src/test/java/AppFeatures/Reviews.feature
@@ -1,6 +1,11 @@
 Feature: Lloyds bank reviews
 
   Scenario: Search for reviews
-  Given : I find all reviews rating
-    When : I have more than one 5star review
-    Then : Show only the 5stars reviews
+    Given : I opened the llyods review page
+    When : I find more than one review
+    Then : Show all reviews
+
+#  Scenario: Search for 5 stars reviews
+#    Given : I opened the llyods review page
+#    When : I find all 5 stars reviews
+#    Then : I show all found reviews
--- src/test/java/StepDefinitions/Ratings.java
+++ src/test/java/StepDefinitions/Ratings.java
@@ -4,7 +4,9 @@ import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -20,80 +22,47 @@ import java.util.concurrent.TimeUnit;
 
 
 public class Ratings {
-    WebDriver driver;
-    Map<String, Object> vars;
-    JavascriptExecutor js;
-    List<String> ReviewsRatings = new ArrayList<>();
+//    WebDriver driver = new ChromeDriver();
+    public static WebDriver driver;
+    List<WebElement> reviews = new ArrayList<>();
 
-    public static String cleanString(
-            String pValue) {
-        if (pValue == null) {
-            return null;
-        } else {
-            if (pValue.contains("\"")) {
-                pValue = pValue.replace("\"", "\"\"");
-            }
-            if (pValue.contains(",")
-                    || pValue.contains("\n")
-                    || pValue.contains("'")
-                    || pValue.contains("\\")
-                    || pValue.contains("\"")) {
-                return "\"" + pValue + "\"";
-            }
-        }
-        return pValue;
+    @Given("^: I opened the llyods review page$")
+    public void iOpenedTheLlyodsReviewPage() {
+//        driver = new ChromeDriver();
+//        driver.get("https://smartmoneypeople.com/lloyds-bank-reviews");
+////        driver.manage().window().setSize(new Dimension(1050, 660));
+//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//        List<WebElement> reviews = driver.findElements(By.className("review"));
+        driver = new ChromeDriver(); //
+        driver.get("https://learn.letskodeit.com/p/practice");
+        driver.manage().window().maximize();
     }
 
-
-    @Given("^: I find all reviews rating$")
-    public void i_find_all_reviews_rating()  {
-        ReviewsRatings.add("hello");
-        // Write code here that turns the phrase above into concrete actions
-        driver.get("https://smartmoneypeople.com/lloyds-bank-reviews");
-        driver.manage().window().setSize(new Dimension(1050, 660));
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        List<WebElement> reviews = driver.findElements(By.className("review"));
-
-        for (WebElement webElement : reviews) {
-            // get all review ratings and find and size of ratings
-            int reviewRating = webElement.findElements(By.cssSelector(".rating .full")).size();
-            ReviewsRatings.add(String.valueOf(reviewRating));
-        }
-
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    @When("^: I find more than one review$")
+    public void iFindMoreThanOneReview() {
+        // find all reviews in page
+        reviews = driver.findElements(By.className("review"));
     }
 
-    @When("^: I have more than one (\\d+)star review$")
-    public void i_have_more_than_one_star_review(int arg1)  {
-        // Write code here that turns the phrase above into concrete actions
-        driver.get("https://smartmoneypeople.com/lloyds-bank-reviews");
-        driver.manage().window().setSize(new Dimension(1050, 660));
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        List<WebElement> reviews = driver.findElements(By.className("review"));
-
-        for (WebElement webElement : reviews) {
-            // get all review ratings and find and size of ratings
-             int reviewRating = webElement.findElements(By.cssSelector(".rating .full")).size();
-            ReviewsRatings.add(String.valueOf(reviewRating));
-        }
-
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    @Then("^: Show all reviews$")
+    public void showAllReviews() {
+        // print all reviews
+        System.out.println(reviews);
     }
 
-    @Then("^: Show only the (\\d+)stars reviews$")
-    public void show_only_the_stars_reviews(int arg1)  {
-        // Write code here that turns the phrase above into concrete actions
-        driver.get("https://smartmoneypeople.com/lloyds-bank-reviews");
-        driver.manage().window().setSize(new Dimension(1050, 660));
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        List<WebElement> reviews = driver.findElements(By.className("review"));
-
-        for (WebElement webElement : reviews) {
-            // get all review ratings and find and size of ratings
-            int reviewRating = webElement.findElements(By.cssSelector(".rating .full")).size();
-            ReviewsRatings.add(String.valueOf(reviewRating));
-        }
-
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    @When("^: I find all (\\d+) stars reviews$")
+    public void iFindAllStarsReviews(int arg0) {
+        //find only stars reviews
     }
+
+    @Then("^: I show all found reviews$")
+    public void iShowAllFoundReviews() {
+        // print only 5 stars reviews
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
 }
